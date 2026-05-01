70c504b2b87b0424f0ae9750faaf573068695c26
Rahman <rahmanuddinmd@gmail.com>
2026-05-02 02:48:01 +0530

    Update HomeController.java

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index"; // loads index.html
+        return "index";
     }
 
     @GetMapping("/about")
